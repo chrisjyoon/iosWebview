a5717ed9b282f0fde297f5954491886f899d983f
chrisyoon <chrsjyoon@gmail.com>
2021-01-15 12:08:08 +1100

    Initial Commit

---
+++ Webview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Webview
+//
+//  Created by Chris Yoon on 15/1/21.
+//  Copyright © 2021 Chris Yoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
